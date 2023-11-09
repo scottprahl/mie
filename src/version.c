3dcd9fe59b7c7126f38c45466c10580a1ccfa454
Scott Prahl <scott@prahls.com>
2023-11-08 16:09:56 -0800

    remove ctangle cruft

--- src/version.c
+++ src/version.c
@@ -2,4 +2,4 @@
 /* version number is set in                   <../Makefile>  */
 /* date is the last modification date for     <../Makefile>  */
 
-char *Version = "2-6-3 (12:47:38 11/08/23)";
+char *Version = "2-6-3 (16:05:22 11/08/23)";
